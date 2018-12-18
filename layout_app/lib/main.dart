fdf95edcfcb8aad9c0ad0ba449dd98c21f60a51b
Alessio Roberto <darthpelo@gmail.com>
2018-12-18 23:58:00 +0100

    Squashed commit of the following:
    commit 6c3f6652d389be58e3a43a2d92760f5c65f1f132
    Author: Alessio Roberto <darthpelo@gmail.com>
    Date:   Tue Dec 18 23:56:36 2018 +0100
    
        Implemented navigation/route example
    
    commit 6f5954a6628348e21b28138f66288cf6a769f1f3
    Author: Alessio Roberto <darthpelo@gmail.com>
    Date:   Tue Dec 18 00:39:59 2018 +0100
    
        Add Stateful widget for the start icon

--- layout_app/lib/main.dart
+++ layout_app/lib/main.dart
@@ -41,11 +41,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -117,6 +113,55 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
           ],
         ),
       ),
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      // If the lake is currently favorited, unfavorite it.
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+        // Otherwise, favorite it.
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0.0),
+          child: IconButton(
+            icon: (_isFavorited
+                ? Icon(Icons.star)
+                : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18.0,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
     );
   }
 }
