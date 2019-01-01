687c464cb129646d12cae11e44009f3295650161
Alessio Roberto <darthpelo@gmail.com>
2019-01-01 12:28:47 +0100

    Add SharedPreferences logic to store search

--- fweather_app/lib/screens/city_search.dart
+++ fweather_app/lib/screens/city_search.dart
@@ -4,27 +4,27 @@ import 'package:google_maps_webservice/places.dart';
 import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
+import 'package:fweather_app/keyschain.dart';
 
-import 'package:fweather_app/fetchModel.dart';
-
-const kGoogleApiKey = "AIzaSyCeHZxMqTNYpSP1L7CLlfuhXBnMLbkRSVQ";
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CitySearch extends StatefulWidget {
   @override
   _CitySearchState createState() => _CitySearchState();
 }
 
-final homeScaffoldKey = GlobalKey<ScaffoldState>();
-final searchScaffoldKey = GlobalKey<ScaffoldState>();
-
 class _CitySearchState extends State<CitySearch> {
   List<String> _cities = [];
 
   @override
+  void initState() {
+    super.initState();
+    _loadCities();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      key: homeScaffoldKey,
       child: CustomScrollView(slivers: <Widget>[
         CupertinoSliverNavigationBar(
           largeTitle: Text("Cities"),
@@ -72,14 +72,24 @@ class _CitySearchState extends State<CitySearch> {
     );
   }
 
-  void _addCity(String name) {
-    _cities.add(name);
+  _loadCities() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _cities= (prefs.getStringList('cities') ?? []);
+    });
+  }
+
+  void _addCity(String name) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _cities = (prefs.getStringList('cities') ?? []);
+      _cities.add(name);
+      prefs.setStringList('cities', _cities);
+    });
   }
   
   void onError(PlacesAutocompleteResponse response) {
-    homeScaffoldKey.currentState.showSnackBar(
-      SnackBar(content: Text(response.errorMessage)),
-    );
+    print(response.errorMessage);
   }
 
   Future<String> _handlePressButton() async {
@@ -87,7 +97,7 @@ class _CitySearchState extends State<CitySearch> {
     // then get the Prediction selected
     Prediction p = await PlacesAutocomplete.show(
       context: context,
-      apiKey: kGoogleApiKey,
+      apiKey: getKey(),
       onError: onError,
       mode: Mode.overlay,
       language: "system",
